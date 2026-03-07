9ab711d8f4bcb0a5cd1c209e381c7c30e8bd2a11
jbs-17 <jatibangun17@gmail.com>
2026-03-07 18:50:00 +0700

    mengubah field bool_deleted ke is_deleted

--- src/api/notes/types.ts
+++ src/api/notes/types.ts
@@ -8,7 +8,7 @@ export interface NoteDoc {
        string_content: string;
        time_created_at: number;
        time_updated_at: number;
-       bool_deleted: boolean;
+       is_deleted: boolean;
        is_dirty: boolean;
        user_id: string;
 }
